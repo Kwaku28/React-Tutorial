2aa2faa3aa6029e1e449d6ae93e5749d746c7ad3
Lawrence Amoafo <rekenburg777@gmail.com>
2023-05-01 15:19:44 +0000

    Descriptive README to highlight project

---
+++ src/modules/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <>
+    <header>
+      <h1>Todo List</h1>
+      <p>Time Blocking made easy</p>
+    </header>
+  </>
+);
+
+export default Header;
